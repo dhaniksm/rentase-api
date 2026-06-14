43d73e334a091141394d9593cddbea6716d5491c
akitra13 <errongaming26@gmail.com>
2026-06-15 00:09:51 +0700

    Fix: Add securitySchemes to Swagger to enable Authorize button

--- swagger.js
+++ swagger.js
@@ -16,6 +16,20 @@ const options = {
         url: "http://localhost:3000",
       },
     ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
   },
   apis: ["./routes/*.js"],
 };
